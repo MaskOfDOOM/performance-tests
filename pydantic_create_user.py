61264a99562b0b25d05feb7a674a823b393be8d9
MaskOfDOOM <128933824+MaskOfDOOM@users.noreply.github.com>
2025-10-14 14:58:05 +0300

    Применяем Pydantic в HTTP API клиентах

--- pydantic_create_user.py
+++ pydantic_create_user.py
@@ -7,12 +7,12 @@ class UserSchema(BaseModel):
     """
     Модель данных пользователя
     """
-    id: str=Field(default_factory=lambda: str(uuid.uuid4()))
-    email: EmailStr=Field(default="johndoe@example.com")
-    last_name: str=Field(alias="lastName", default="Doe")
-    first_name: str=Field(alias="firstName", default="John")
-    middle_name: str=Field(alias="middleName", default="Olegovich")
-    phone_number: str=Field(alias="phoneNumber", default="88005553535")
+    id: str = Field(default_factory=lambda: str(uuid.uuid4()))
+    email: EmailStr = Field(default="johndoe@example.com")
+    last_name: str = Field(alias="lastName", default="Doe")
+    first_name: str = Field(alias="firstName", default="John")
+    middle_name: str = Field(alias="middleName", default="Olegovich")
+    phone_number: str = Field(alias="phoneNumber", default="88005553535")
 
 user_default_model = UserSchema(
     id="84226095-6a24-4e45-9e83-383a477f9fe4",
@@ -29,11 +29,11 @@ class CreateUserRequestSchema(BaseModel):
     """
     Модель данных для запроса на создание пользователя
     """
-    email: EmailStr=Field(default="johndoe@example.com")
-    last_name: str=Field(alias="lastName", default="Doe")
-    first_name: str=Field(alias="firstName", default="John")
-    middle_name: str=Field(alias="middleName", default="Olegovich")
-    phone_number: str=Field(alias="phoneNumber", default="88005553535")
+    email: EmailStr = Field(default="johndoe@example.com")
+    last_name: str = Field(alias="lastName", default="Doe")
+    first_name: str = Field(alias="firstName", default="John")
+    middle_name: str = Field(alias="middleName", default="Olegovich")
+    phone_number: str = Field(alias="phoneNumber", default="88005553535")
 
 create_user_request_model = CreateUserRequestSchema(
     email="johndoe@example.com",
