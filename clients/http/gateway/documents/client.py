084fca612671d2c94a191413d64f72887e82cc1a
MaskOfDOOM <128933824+MaskOfDOOM@users.noreply.github.com>
2025-10-27 19:38:56 +0300

    Подготовка HTTP API клиента для Locust

--- clients/http/gateway/documents/client.py
+++ clients/http/gateway/documents/client.py
@@ -1,6 +1,10 @@
 from clients.http.client import HTTPClient, HTTPClientExtensions
 from httpx import Response
-from clients.http.gateway.client import build_gateway_http_client
+from locust.env import Environment
+from clients.http.gateway.client import (
+    build_gateway_http_client, 
+    build_gateway_locust_http_client
+)
 from clients.http.gateway.documents.schema import (
     GetTariffDocumentResponseSchema,
     GetContractDocumentResponseSchema
@@ -49,3 +53,16 @@ def build_documents_gateway_http_client() -> DocumentsGatewayHTTPClient:
     :return: Готовый к использованию DocumentsGatewayHTTPClient.
     """
     return DocumentsGatewayHTTPClient(client=build_gateway_http_client())
+
+
+def build_documents_gateway_locust_http_client(environment: Environment) -> DocumentsGatewayHTTPClient:
+    """
+    Функция создаёт экземпляр DocumentsGatewayHTTPClient, адаптированного под locust.
+
+    Клиент автоматически собирает метрики и передаёт их в Locust через хуки.
+    Используется исключительно в нагрузочных тестах.
+
+    :param environment: объект окружения Locust.
+    :return: экземпляр DocumentsGatewayHTTPClient с хуками сбора метрик.
+    """
+    return DocumentsGatewayHTTPClient(client=build_gateway_locust_http_client(environment))
