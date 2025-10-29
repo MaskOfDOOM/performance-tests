c4e7b2c276757f5f38190bc6a30024f9eb08ba91
MaskOfDOOM <128933824+MaskOfDOOM@users.noreply.github.com>
2025-10-29 14:34:13 +0300

    Использование HTTP API клиентов в нагрузочных сценариях

--- locust_open_debit_card_account.py
+++ locust_open_debit_card_account.py
@@ -1,31 +1,25 @@
-from locust import HttpUser, between, task
-from tools.fakers import fake
+from locust import User, between, task
+from clients.http.gateway.accounts.client import build_accounts_gateway_locust_http_client
+from clients.http.gateway.users.client import UsersGatewayHTTPClient, build_users_gateway_locust_http_client
+from clients.http.gateway.users.schema import CreateUserResponseSchema
 
 
-class OpenDebitCardAccountScenarioUser(HttpUser):
+class OpenDebitCardAccountScenarioUser(User):
+    host = "localhost"
     wait_time = between(1, 3)
-    user_data: dict
+    users_gateway_client = UsersGatewayHTTPClient
+    create_user_response = CreateUserResponseSchema
 
 
     def on_start(self) -> None:
-        request = {
-            "email": fake.email(),
-            "lastName": fake.last_name(),
-            "firstName": fake.first_name(),
-            "middleName": fake.middle_name(),
-            "phoneNumber": fake.phone_number()
-        }
-        response = self.client.post("/api/v1/users", json=request)
-
-        self.user_data = response.json()
+        self.users_gateway_client = build_users_gateway_locust_http_client(self.environment)
+        
+        self.create_user_response = self.users_gateway_client.create_user()
 
 
     @task
     def open_debit_card_account(self) -> None:
-        request = {
-            "userId": self.user_data['user']['id']
-        }
-        response = self.client.post("/api/v1/accounts/open-debit-card-account", json=request)
+        self.accounts_gateway_client = build_accounts_gateway_locust_http_client(self.environment)
 
-        self.account_data = response.json()
+        self.accounts_gateway_client.open_debit_card_account(self.create_user_response.user.id)
         
