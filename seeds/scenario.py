75e5b911c2458f5624c637b189d4478cd1b53b6f
MaskOfDOOM <128933824+MaskOfDOOM@users.noreply.github.com>
2025-11-12 12:45:10 +0300

    Добавляем логирование для нагрузочных тестов

--- seeds/scenario.py
+++ seeds/scenario.py
@@ -1,9 +1,12 @@
 from abc import ABC, abstractmethod
+from logging import getLogger
 
 from seeds.builder import build_grpc_seeds_builder
 from seeds.dumps import load_seeds_result, save_seeds_result
 from seeds.schema.plan import SeedsPlan
 from seeds.schema.result import SeedsResult
+
+logger = getLogger("SEEDS_SCENARIO")
 
 
 class SeedsScenario(ABC):
@@ -42,18 +45,27 @@ class SeedsScenario(ABC):
         Сохраняет результат сидинга в файл.
         :param result: Объект SeedsResult, содержащий сгенерированные данные.
         """
+        logger.info(f"[{self.scenario}] Savings seeding result to file.")
         save_seeds_result(result=result, scenario=self.scenario)
+        logger.info(f"[{self.scenario}] Seeding result saved successfully.")
 
     def load(self) -> SeedsResult:
         """
         Загружает результаты сидинга из файла.
         :return: Объект SeedsResult, содержащий данные, загруженные из файла.
         """
-        return load_seeds_result(scenario=self.scenario)
+        logger.info(f"[{self.scenario}] Loading seeding from file.")
+        result =  load_seeds_result(scenario=self.scenario)
+        logger.info(f"[{self.scenario}] Seeding result loaded successfully")
+        return result
+    
     
     def build(self) -> None:
         """
         Генерирует данные с помощью билдера, используя план сидинга, и сохраняет результат.
         """
+        plan_json = self.plan.model_dump_json(indent=2, exclude_defaults=True)
+        logger.info(f"[{self.scenario}] Starting seeding data generation for plan: {plan_json}")
         result = self.builder.build(self.plan)
+        logger.info(f"[{self.scenario}] Seeding data generation completed.")
         self.save(result)
